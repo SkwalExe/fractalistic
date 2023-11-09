827bcddcdc350856fa4f53c76e9f44a9f6678512
Skwal <skwal@etik.com>
2023-11-09 15:59:19 +0100

    add class for increment commands

--- fractalistic/command.py
+++ fractalistic/command.py
@@ -11,3 +11,59 @@ class Command():
         self.help = help
         self.accepted_arg_count = accepted_arg_counts
         self.extra_help = extra_help
+
+class CommandIncrementArgParseResult():
+    success: bool = False
+    error_message: str | None = None
+    new_value: int | float | None = None
+
+
+class CommandIncrement(Command):
+    min_value: int | None
+    max_value: int | None
+    app_attribute: str
+
+    def __init__(self, app_attribute: str, min_value = None, max_value = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.max_value = max_value
+        self.min_value = min_value
+        self.app_attribute = app_attribute
+
+
+
+    def parse_args(self, current_attrib_value, args):
+        """Returns the new value if the args are valid, else None"""
+        result = CommandIncrementArgParseResult()
+        if len(args) == 1:
+            try:
+                result.new_value = int(args[0])
+            except ValueError:
+                result.error_message = "[red]The value must be an integer"
+                return result
+        
+        elif len(args) == 2:
+            sign = args[0]
+            value = args[1]
+
+            if not sign in ["+", "-"]:
+                result.error_message = f"[red]Unknown sign: {sign}"
+                return result
+            
+            try:
+                value = int(value)
+            except ValueError:
+                result.error_message = "[red]The increment value must be an integer"
+                return result
+            
+            result.new_value = current_attrib_value + (value if sign == "+" else -value)
+
+        if not self.min_value is None and result.new_value < self.min_value:
+            result.error_message = f"[red]The new value must be greater than {self.min_value}"
+            return result
+
+        if not self.max_value is None and result.new_value > self.max_value:
+            result.error_message = f"[red]The new value must not be greater than {self.max_value}"
+            return result
+        
+        result.success = True
+        return result
